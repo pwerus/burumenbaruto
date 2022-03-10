61ebb8832f5bd8070f939d3981ad3973e071141a
arthur kenji <arthurhaguiuda@gmail.com>
2022-03-10 18:44:59 -0300

    Mudei aqui pro pwerus safado

--- poupanca.js
+++ poupanca.js
@@ -3,7 +3,7 @@ var prompt = require("prompt-sync")();
 var deposito = parseFloat(prompt("Insira a quantia a ser depositada: "))
 var tempo = parseFloat(prompt("Quantos meses deixará guardado o dinheiro? "))
 
-var porcentagem = 10 / 100 * deposito
+var porcentagem = 0.05 / 100 * deposito
 var lucro = porcentagem * tempo + deposito
 
 console.log("O lucro foi de " + lucro)
